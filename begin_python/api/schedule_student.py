4834bb4f7bfb4e0b498dc4522f1b8a3b5dd9c726
SULTON WAEKACHI <6110110107@psu.ac.th>
2022-03-02 21:49:42 +0700

    123

--- begin_python/api/schedule_student.py
+++ begin_python/api/schedule_student.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python
 # -- coding: utf-8 --
+from ast import literal_eval
 from dbConfig import *
+import json
 
 
 @app.route('/schedule_student', methods=['POST'])
 def schedule_student():
     try:
         data = request.json
-        room_id = data["room_id"]
+        room = data["room"]
+        result = []
 
         # # connect db
         conn = connectToDB()
         cursor = conn.cursor()
 
-        sql = """ SELECT * FROM schedule_student WHERE room_id = '""" + str(room_id) + """'"""
-        # print(sql)
+        sql = " SELECT * FROM schedule_student WHERE room = '" + str(room) +"'"
+        print(sql)
         cursor.execute(sql)
         data_sql = cursor.fetchall()
         columns = [column[0] for column in cursor.description]
-        # print(data_sql)
-        # print(columns)
         result = toJson(data_sql,columns)
         if len(result) > 0:
+            result[0]['schedule'] =  literal_eval(result[0]['schedule'])
             result = {"status":"OK","result":result}
         else:
             result = {"status":"ER","errorMessage":"ไม่พบ ข้อมูล"}
